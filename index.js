04256b4c60b6eb4ec1c54059dc1316c1a211e687
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2020-07-02 03:02:55 +0600

    Added basic graphql support

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-'use strict'
-const express = require('express')
+"use strict";
+const express = require("express");
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 // Create the express app
-const app = express()
+const app = express();
 
-// Routes and middleware
-// app.use(/* ... */)
-// app.get(/* ... */)
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 // Error handlers
-app.use(function fourOhFourHandler (req, res) {
-  res.status(404).send()
-})
-app.use(function fiveHundredHandler (err, req, res, next) {
-  console.error(err)
-  res.status(500).send()
-})
+app.use(function fourOhFourHandler(req, res) {
+  res.status(404).send();
+});
+app.use(function fiveHundredHandler(err, req, res, next) {
+  console.error(err);
+  res.status(500).send();
+});
 
 // Start server
 app.listen(1234, function (err) {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
 
-  console.log('Started at http://localhost:1234')
-})
+  console.log("Started at http://localhost:1234");
+});
